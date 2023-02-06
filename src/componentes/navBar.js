108210ead031587812b143b2efb79b005282f80c
NicoB <nicobasma_@hotmail.com>
2023-02-06 19:22:26 +0000

    exercise by module

--- src/componentes/navBar.js
+++ src/componentes/navBar.js
@@ -1,5 +1,4 @@
 const navBar = document.querySelector("#main-nav");
-const body = document.querySelector("#main-body");
 
 navBar.innerHTML = `<nav class="navbar navbar-expand-lg bg-body-tertiary">
 <div class="container-fluid">
@@ -18,29 +17,3 @@ navBar.innerHTML = `<nav class="navbar navbar-expand-lg bg-body-tertiary">
     </div>
 </div>
 </nav>`
-
-const todoList = `<div class="card mb-5" style="width: 18rem;">
-<div class="card-body">
-    <div class="mb-3">
-        <input type="title" class="form-control" name="title" placeholder="TODO">
-    </div>
-    <div class="mb-3">
-        <input type="date" class="form-control" name="date" placeholder="">
-    </div>
-    <div class="mb-3">
-        <textarea class="form-control" name="description" rows="3"></textarea>
-    </div>
-    <div class="mb-3">
-        <div class="btn-group w-100" role="group" aria-label="Basic mixed styles example">
-            <button type="button" class="btn btn-danger">URGENTE</button>
-            <button type="button" class="btn btn-success">Hecho</button>
-        </div>
-    </div>
-</div>
-</div>`
-
-body.innerHTML = `
-<div class="container bg-light-subtle d-flex flex-column align-items-center">
-    <h1 class="text-center mb-5">TO-DO</h1>
-    ${todoList}
-</div>`
